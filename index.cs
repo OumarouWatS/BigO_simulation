a0a4ea34d1c88133346c8a2318ce377b3de406ec
Salome Oumarou Wate <94982651+OumarouWatS@users.noreply.github.com>
2023-01-24 21:41:51 -0600

    Update index.cs

--- index.cs
+++ index.cs
@@ -1,10 +1,33 @@
-namespace DSA_Homework2
+namespace Homework2
 {
     using System;
     using System.Diagnostics;
     internal class Program
     {
-        public static int max1 = 100000, max2 = 1000000, max3 = 10000000, max4 = 100000000, max5 = 1000000000;
+        public static int start = 10000, N2 = 100000, N3 = 1000000, N4 = 10000000, N5 = 100000000, N6 = 1000000000;
+
+        // Disjoint array filler
+        public static int[] arrayFiller(int[] myArray, int start, int N)
+        {
+            for(int i=start; i <= N; i++)
+            {
+                myArray[i] = i+1;
+            }
+
+            return myArray;
+        }
+
+        // generates total_Ns number of Ns in the powers of 10 from start
+        public static int[] N_generator(int start, int total_Ns)
+        {
+            int[] myArray = new int[total_Ns];
+            for (int i=0; i<total_Ns; i++)
+            {
+                myArray[i] = start;
+                start *= 10;
+            }
+            return myArray;
+        }
 
         public static bool disjoint1(int[] groupA, int[] groupB, int[] groupC)
         {
@@ -27,68 +50,53 @@ namespace DSA_Homework2
             return true;
         }
 
-        public static void random_arrays(int[] array_one, int[] array_two, int[] array_three, int[] array_four, int[] array_five)
-        {
-            Random rand = new Random();
-            for (int i = 0; i < max1; i++)
-            {
-                int a = rand.Next(500);
-                array_one[i] = a;
-            }
-            for (int i = 0; i < max2; i++)
-            {
-                int a = rand.Next(500);
-                array_two[i] = a;
-            }
-            for (int i = 0; i < max3; i++)
-            {
-                int a = rand.Next(500);
-                array_three[i] = a;
-            }
-            for (int i = 0; i < max4; i++)
-            {
-                int a = rand.Next(500);
-                array_four[i] = a;
-            }
-            for (int i = 0; i < max5; i++)
-            {
-                int a = rand.Next(500);
-                array_five[i] = a;
-            }
-        }
 
         static void Main(string[] args)
         {
-            int[] array_one = new int[max1];
-            int[] array_two = new int[max2];
-            int[] array_three = new int[max3];
-            int[] array_four = new int[max4];
-            int[] array_five = new int[max5];
+            int N = 5;
+
+            int[] arrayOfNs = new int[N];
+            arrayOfNs = N_generator(start, N);;
+
+            int[][] group1 = new int[3][];
+
+            // creates and initializes 3 groups of size arrayOfNs[0]
+            int j = 0;
+            int z = arrayOfNs[0];
+            for (int i = 0; i < group1.Length; i++)
+            {
+                group1[i] = new int[arrayOfNs[0]];
+                group1[i] = arrayFiller(group1[i], j, z);
+                j += arrayOfNs[0];
+                z += arrayOfNs[0];
+            }
+
+
             Stopwatch stopWatch = new Stopwatch();
-
-            random_arrays(array_one, array_two, array_three, array_four, array_five);
-
-
-            stopWatch.Start();
-            disjoint1(array_four, array_five, array_three);
-            stopWatch.Stop();
-
-            
-            stopWatch.Start();
-            disjoint2(array_four, array_five, array_three);
-            stopWatch.Stop();
-            
-
             TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime;
+            string elapsedTime1, elapsedTime2;
 
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+
+            stopWatch.Start();
+            disjoint1(group1[0], group1[1], group1[2]);
+            stopWatch.Stop();
+
+            elapsedTime1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime1);
 
-            Console.WriteLine("RunTime " + elapsedTime);
+
+            stopWatch.Start();
+            disjoint1(group1[0], group1[1], group1[2]);
+            stopWatch.Stop();
+
+            elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime2);
 
         }
     }
-    
+
 }
